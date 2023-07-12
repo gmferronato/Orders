68ff8926a1c34f7764e19aa932aa13b592bddaa0
Gian Marco <gmferronato@gmail.com>
2023-07-12 10:17:11 +0200

    Added another line of comment

--- Forecast.java
+++ Forecast.java
@@ -4,6 +4,7 @@ public class Forecast {
     public static void main(String[] args) {
 
         /* This is the new balance */
+        /* Waiting for a date though...*/
 
         int balance = 1809;
         System.out.println(balance);
